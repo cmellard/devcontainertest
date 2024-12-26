c5d6b7b13242a78cdf2cd022378f9b0093c77473
Chris <chris@dont.git.blame.me>
2024-12-26 13:41:43 +1300

    hacking

--- .devcontainer/post-create-setup.sh
+++ .devcontainer/post-create-setup.sh
@@ -64,6 +64,8 @@ END
 echo "Stopping PostgreSQL..."
 sudo -u postgres /usr/lib/postgresql/14/bin/pg_ctl -D "$DATA_DIR" stop
 
+export PATH="$PATH:/usr/local/go/bin:/go/bin"
+
 #/root/.rvm/rubies/ruby-3.3.6/bin/bundle config set --global path /workspaces/gems
 
 echo "Post-create setup completed successfully."
